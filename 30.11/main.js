fc2ee322c8e6efa4a78cb24361560ed26e3beb51
alex-sikach <hlophlop1245@gmail.com>
2021-12-01 14:01:29 +0200

    30.11

---
+++ 30.11/main.js
@@ -0,0 +1,50 @@
+class State {
+	constructor(options) {
+		this.backgroundColor = options.backgroundColor;
+		this.border = options.border;
+		this.borderRadius = options.borderRadius;
+	}
+}
+const green_state = new State({backgroundColor:'#6ace61',border:'10px solid #1a1a1a',borderRadius:'20%'});
+const blue_state = new State({ backgroundColor:'#617ace',border:'15px solid #fff',borderRadius:'30%'});
+const red_state = new State({backgroundColor:'#f76f6f',border:'5px solid #000',borderRadius:'40%'});
+const none_state = new State({backgroundColor:'#fff',border:'none',borderRadius:'50%'});
+const states = [green_state, blue_state, red_state];
+const NONE_STATE = 'none_-_state';
+let state = NONE_STATE;
+function stop() {
+	setProperties(document.querySelector('.el').style, none_state);
+}
+stop();
+let counter = 0;
+function setProperties(el = document.querySelector('.el').style, stateDef=null) {
+	if (stateDef == null) {
+		const index = counter % states.length;//if(counter > 2)const index=counter;counter++;
+		state = states[index];
+		for (const key in state) {
+			let val = state[key];
+			el[key] = val;
+		}
+		counter++;
+	} else {
+		const state = stateDef;
+		for (const key in state) {
+			let val = state[key];
+			el[key] = val;
+		}
+	}
+}
+let inter;
+function interFunc() {
+	setProperties();
+	inter = setInterval(setProperties, 700);
+}
+document.querySelector('.el').onclick = () => {
+	if (states.includes(state)) {
+		state = NONE_STATE;
+		stop();
+		clearInterval(inter);
+	} else {
+		interFunc();
+	}
+}
